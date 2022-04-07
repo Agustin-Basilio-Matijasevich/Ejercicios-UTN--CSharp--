007b0d9267b0ca40cdec6c6093af62249f182386
Agustin-Basilio-Matijasevich <agustinbasiliomatijasevich@gmail.com>
2022-04-07 19:28:29 -0300

    Adaptacion multi ide y Creacion Primer Proyecto consola

---
+++ PROG3/Monodevelop/Primer_Clase/Primer_Clase/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Primer_Clase
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
