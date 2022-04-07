b3ced3cde887ed677763211e384e187d4f501c3f
Agustin-Basilio-Matijasevich <agustinbasiliomatijasevich@gmail.com>
2022-04-07 19:49:57 -0300

    App que mueve el cursor

---
+++ PROG3/Monodevelop/Primer_Clase/CursorMoves/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+
+namespace CursorMoves
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            for (int i=0; i < 10; i++)
+            {
+                Thread.Sleep(1000);
+                Console.SetCursorPosition(i*2, i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
