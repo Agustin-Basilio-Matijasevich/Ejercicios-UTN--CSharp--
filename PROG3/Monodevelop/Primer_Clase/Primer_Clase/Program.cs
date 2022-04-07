cd924ffc192ef9b58b398a2557f8f5b353f4c364
Agustin-Basilio-Matijasevich <agustinbasiliomatijasevich@gmail.com>
2022-04-07 19:33:38 -0300

    Aplicacion Cuatro Operaciones Basicas

--- PROG3/Monodevelop/Primer_Clase/Primer_Clase/Program.cs
+++ PROG3/Monodevelop/Primer_Clase/Primer_Clase/Program.cs
@@ -6,7 +6,19 @@ namespace Primer_Clase
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n1, n2;
+
+            Console.WriteLine("Ingrese un Numero ENTERO");
+            n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese otro Numero ENTERO");
+            n2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Las cuatro Operaciones Basicas son:\n\n");
+            Console.WriteLine("Suma: {0}", n1 + n2);
+            Console.WriteLine("Resta: {0}", n1 - n2);
+            Console.WriteLine("Multiplicacion: {0}", n1 * n2);
+            Console.WriteLine("DIvision: {0}", n1 / n2);
+
         }
     }
 }
