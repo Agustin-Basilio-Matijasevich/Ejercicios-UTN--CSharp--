d1e20fd05fd3da9ae233d03c01960025d786061d
Agustin-Basilio-Matijasevich <agustinbasiliomatijasevich@gmail.com>
2022-04-07 21:36:10 -0300

    Update Program.cs

--- PROG3/Monodevelop/Primer_Clase/Primer_Clase/Program.cs
+++ PROG3/Monodevelop/Primer_Clase/Primer_Clase/Program.cs
@@ -6,17 +6,17 @@ namespace Primer_Clase
     {
         public static void Main(string[] args)
         {
-            int n1, n2;
+            double n1, n2; //Con Punto flotante soporta dividir por 0
 
             Console.WriteLine("Ingrese un Numero ENTERO");
             n1 = int.Parse(Console.ReadLine());
             Console.WriteLine("Ingrese otro Numero ENTERO");
-            n2 = int.Parse(Console.ReadLine());
+            n2 = int.Parse(Console.ReadLine()); //Probar Tryparse
 
             Console.WriteLine("Las cuatro Operaciones Basicas son:\n\n");
-            Console.WriteLine("Suma: {0}", n1 + n2);
+            Console.WriteLine($"Suma: {n1 + n2}"); //Al usar el signo peso se identifican las salidas de variables, sirve para calculo simple(Interpolacion de Strings)
             Console.WriteLine("Resta: {0}", n1 - n2);
-            Console.WriteLine("Multiplicacion: {0}", n1 * n2);
+            Console.WriteLine("Multiplicacion: " + n1 * n2); //Tambien se puede concatenar como en java
             Console.WriteLine("DIvision: {0}", n1 / n2);
 
         }
