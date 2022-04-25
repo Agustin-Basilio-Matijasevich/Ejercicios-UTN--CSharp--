688c323766810918e6f98336f7e972a693f47829
Agustin-Basilio-Matijasevich <agustinbasiliomatijasevich@gmail.com>
2022-04-25 17:53:01 -0300

    Creacion Proyecto Moda

---
+++ PROG3/Monodevelop/Calculo_Moda-Estadistica/Calculo_Moda-Estadistica/Functions/ModaE.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Calculo_ModaEstadistica.Functions
+{
+    public class ModaType
+    {
+
+    }
+
+    public static class ModaE
+    {
+        public static ModaType ModaCalc()
+        {
+
+        }
+
+    }
+}
---
+++ PROG3/Monodevelop/Calculo_Moda-Estadistica/Calculo_Moda-Estadistica/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Calculo_ModaEstadistica
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
