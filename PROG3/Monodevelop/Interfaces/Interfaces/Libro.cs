38353da92831adbf40f64f1f65fe707a7df0a77a
Agustin-Basilio-Matijasevich <agustinbasiliomatijasevich@gmail.com>
2022-05-25 20:00:42 -0300

    Inicio TP Interfaces Prog

---
+++ PROG3/Monodevelop/Interfaces/Interfaces/Libro.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Interfaces
+{
+    public class Libro : IComparable, IColeccionable
+    {
+        //Campos
+        private string autor, titulo;
+
+        //Propiedades
+        public String Autor { get; set; }
+        public String Titulo { get; set; }
+
+        //Metodos
+        public Libro()
+        {
+
+        }
+
+        public void CompareTo()
+        {
+
+        }
+
+        public void Describir()
+        {
+
+        }
+
+    }
+}
