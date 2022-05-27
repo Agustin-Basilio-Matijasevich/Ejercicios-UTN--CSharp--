2663b70f8d843639fc302b32bea68413124159a2
Agustin-Basilio-Matijasevich <agustinbasiliomatijasevich@gmail.com>
2022-05-27 18:37:02 -0300

    Hash Test

---
+++ LAB3/MonoDevelop/HashTest/HashTest/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace HashTest
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un texto");
+            String T1 = Console.ReadLine();
+            Console.Write("El HASH del Texto es: ");
+            String H1 = BitConverter.ToString(SHA256.Create().ComputeHash(Encoding.UTF8.GetBytes(T1))).Replace("-", string.Empty);
+            Console.WriteLine(H1);
+
+            Console.WriteLine("Ingrese un texto");
+            String T2 = Console.ReadLine();
+            Console.Write("El HASH del Texto es: ");
+            String H2 = BitConverter.ToString(SHA256.Create().ComputeHash(Encoding.UTF8.GetBytes(T2))).Replace("-", string.Empty);
+            Console.WriteLine(H2);
+
+            Console.WriteLine($"\n\n¿Los HASH son iguales?: {H1.Equals(H2)}");
+
+        }
+    }
+}
