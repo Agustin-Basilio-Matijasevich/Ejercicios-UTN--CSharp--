b341c36d6e92f5c404578fe6d51e009f4fc96c30
Agustin-Basilio-Matijasevich <agustinbasiliomatijasevich@gmail.com>
2022-08-24 15:51:05 -0300

    Primer Prueba ASPNET

---
+++ LAB4/Prueba-ASPNET_Clase2LAB4/Prueba-ASPNET_Clase2LAB4/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Prueba_ASPNET_Clase2LAB4.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Frase = "Esto lo escribio el controlador";
+            return View();
+        }
+
+        public ActionResult Saludo()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ LAB4/Prueba-ASPNET_Clase2LAB4/Prueba-ASPNET_Clase2LAB4/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Prueba_ASPNET_Clase2LAB4.MvcApplication" Language="C#" %>
